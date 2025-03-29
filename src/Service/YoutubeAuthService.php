c4abedfd0126f358a4164d820429dbea9c674576
Evgeniy Pavlenko <pavlenkoevgeniy85@gmail.com>
2025-03-29 04:10:45 +0200

    refactor panther

--- src/Service/YoutubeAuthService.php
+++ src/Service/YoutubeAuthService.php
@@ -8,50 +8,50 @@ use Symfony\Component\Panther\Client;
 class YoutubeAuthService
 {
     private string $cookiesPath;
+    private Client $client;
 
-    public function __construct(string $projectDir)
+    public function __construct(string $projectDir,  Client $client)
     {
         $this->cookiesPath = $projectDir . '/var/google-chrome/youtube_cookies.txt';
+        $this->$client = $client;
     }
 
     public function authenticate(string $email, string $password): string
     {
-        $client = Client::createChromeClient();
-
         try {
             // Переходим на YouTube
-            $client->request('GET', 'https://www.youtube.com');
+            $this->client->request('GET', 'https://www.youtube.com');
 
             // Ждем кнопку входа и кликаем
-            $client->waitFor('#avatar-btn');
-            $client->getCrawler()->filter('#avatar-btn')->click();
+            $this->client->waitFor('#avatar-btn');
+            $this->client->getCrawler()->filter('#avatar-btn')->click();
 
             // Ждем появление формы входа
-            $client->waitFor('input[type="email"]');
+            $this->client->waitFor('input[type="email"]');
 
             // Заполняем email
-            $client->getCrawler()->filter('input[type="email"]')->sendKeys($email);
-            $client->getCrawler()->filter('#identifierNext button')->click();
+            $this->client->getCrawler()->filter('input[type="email"]')->sendKeys($email);
+            $this->client->getCrawler()->filter('#identifierNext button')->click();
 
             // Ждем поле пароля
-            $client->waitFor('input[type="password"]', 10);
+            $this->client->waitFor('input[type="password"]', 10);
 
             // Заполняем пароль
-            $client->getCrawler()->filter('input[type="password"]')->sendKeys($password);
-            $client->getCrawler()->filter('#passwordNext button')->click();
+            $this->client->getCrawler()->filter('input[type="password"]')->sendKeys($password);
+            $this->client->getCrawler()->filter('#passwordNext button')->click();
 
             // Ждем завершения входа (появление аватара)
-            $client->waitFor('#avatar-btn', 15);
+            $this->client->waitFor('#avatar-btn', 15);
 
             // Получаем cookies и сохраняем в файл
-            $cookies = $client->getCookieJar()->all();
+            $cookies = $this->client->getCookieJar()->all();
             $this->saveCookies($cookies);
 
             return $this->cookiesPath;
         } catch (\Exception $e) {
             throw new \RuntimeException('YouTube authentication failed: ' . $e->getMessage());
         } finally {
-            $client->quit();
+            $this->client->quit();
         }
     }
 
