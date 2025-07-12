496ea1d4fc8b5c6e17f493cb0a98d57c16ff1c22
Evgeniy Pavlenko <pavlenkoevgeniy85@gmail.com>
2025-07-12 15:02:28 +0200

    add constraints for source edit

--- src/Form/SourceType.php
+++ src/Form/SourceType.php
@@ -6,8 +6,13 @@ namespace App\Form;
 
 use App\Entity\Source;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class SourceType extends AbstractType
 {
@@ -15,10 +20,35 @@ class SourceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('filename')
-            ->add('filepath')
-            ->add('description')
-        ;
+            ->add('filename', TextType::class, [
+                'label'       => 'Filename',
+                'required'    => true,
+                'trim'        => true,
+                'constraints' => [
+                    new Length([
+                        'min' => 5,
+                        'max' => 255,
+                    ]),
+                    new NotBlank([
+                        'normalizer' => 'trim',
+                    ]),
+                    new NotNull(),
+                ],
+            ])
+            ->add('filepath', TextType::class, [
+                'label' => 'Filepath',
+                'attr'  => [
+                    'readonly' => true,
+                ],
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr'  => [
+                    'rows' => 5,
+                ],
+                'required' => false,
+            ]);
     }
 
     #[\Override]
