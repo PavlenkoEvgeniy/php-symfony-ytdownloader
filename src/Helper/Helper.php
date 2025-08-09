4233cbd3bc91c3fd252af0203df6b5765a6839f4
Evgeniy Pavlenko <pavlenkoevgeniy85@gmail.com>
2025-08-09 21:33:10 +0200

    refactor disk checker move to helper

--- src/Helper/Helper.php
+++ src/Helper/Helper.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Helper;
 
-class Helper
+final class Helper
 {
     /**
      * @psalm-suppress InvalidOperand
@@ -20,5 +20,19 @@ class Helper
         $bytes /= (1 << (10 * $pow));
 
         return sprintf('%.1f %s', round($bytes, $precision), $units[$pow]);
+    }
+
+    public static function getFreeSpace(): array
+    {
+        $free  = disk_free_space('/');
+        $total = disk_total_space('/');
+        $used  = $total - $free;
+
+        return [
+            'free'       => Helper::formatBytes($free),
+            'used'       => Helper::formatBytes($used),
+            'total'      => Helper::formatBytes($total),
+            'percentage' => round(($used / $total) * 100, 2),
+        ];
     }
 }
