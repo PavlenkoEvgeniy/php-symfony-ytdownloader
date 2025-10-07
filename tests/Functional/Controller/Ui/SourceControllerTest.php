e751e81558992f63feb448bda7484748a02942a7
Evgeniy Pavlenko <pavlenkoevgeniy85@gmail.com>
2025-10-07 09:04:07 -0300

    add functional test for source controller

---
+++ tests/Functional/Controller/Ui/SourceControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Controller\Ui;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class SourceControllerTest extends WebTestCase
+{
+    private UserRepository $userRepository;
+    private KernelBrowser $client;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        $this->userRepository = $this->getContainer()->get(UserRepository::class);
+    }
+
+    public function testSourcePageIsOpeningOk(): void
+    {
+        $user = $this->userRepository->findOneByEmail('admin@admin.local');
+        $this->client->loginUser($user);
+
+        $this->client->request('GET', '/ui/source');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('a', '+ Add new download');
+    }
+}
