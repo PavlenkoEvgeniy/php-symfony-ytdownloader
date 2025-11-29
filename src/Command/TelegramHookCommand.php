b879b9e3421e49bda1fb19221f93522a9c1f2b22
Pavlenko Evgeniy <pavlenkoevgeniy85@gmail.com>
2025-11-29 13:35:46 -0500

    update command name

--- src/Command/TelegramHookCommand.php
+++ src/Command/TelegramHookCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
-    name: 'telegram:hook',
+    name: 'app:telegram:hook',
     description: 'Add a webhook for the Telegram bot',
 )]
 final class TelegramHookCommand extends Command
--- src/Command/TelegramUnhookCommand.php
+++ src/Command/TelegramUnhookCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
-    name: 'telegram:unhook',
+    name: 'app:telegram:unhook',
     description: 'Remove a webhook for the Telegram bot',
 )]
 final class TelegramUnhookCommand extends Command
