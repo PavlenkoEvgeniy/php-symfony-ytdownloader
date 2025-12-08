9c9ea645e1a27c6b43be3a7f6ecdb47e5bf64fff
Pavlenko Evgeniy <pavlenkoevgeniy85@gmail.com>
2025-12-08 03:01:23 +0200

    rename video downloader service

--- src/MessageHandler/DownloadMessageHandler.php
+++ src/MessageHandler/DownloadMessageHandler.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace App\MessageHandler;
 
 use App\Message\DownloadMessage;
-use App\Service\VideoDownloadService;
+use App\Service\VideoDownloaderService;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 final readonly class DownloadMessageHandler
 {
-    public function __construct(private VideoDownloadService $processYoutubeVideo)
+    public function __construct(private VideoDownloaderService $processYoutubeVideo)
     {
     }
 
