09691a2a4ed6d634bd4589f0918fa2d758e93d03
Pavlenko Evgeniy <pavlenkoevgeniy85@gmail.com>
2026-01-10 21:47:09 +0200

    fix phpstan issue

--- tests/Helper/HelperTest.php
+++ tests/Helper/HelperTest.php
@@ -10,51 +10,19 @@ use PHPUnit\Framework\TestCase;
 final class HelperTest extends TestCase
 {
     /**
-     * @return float[]
+     * @return array<int, array{int|float|null, int, string}>
      */
     public function bytesProvider(): array
     {
         return [
-            [
-                'bytes'     => null,
-                'precision' => 2,
-                'expected'  => '0.0 B',
-            ],
-            [
-                'bytes'     => -1,
-                'precision' => 2,
-                'expected'  => '0.0 B',
-            ],
-            [
-                'bytes'     => 0,
-                'precision' => 2,
-                'expected'  => '0.0 B',
-            ],
-            [
-                'bytes'     => 1000,
-                'precision' => 2,
-                'expected'  => '1000.0 B',
-            ],
-            [
-                'bytes'     => 1234,
-                'precision' => 2,
-                'expected'  => '1.2 KB',
-            ],
-            [
-                'bytes'     => 2345678,
-                'precision' => 2,
-                'expected'  => '2.2 MB',
-            ],
-            [
-                'bytes'     => 12345678912,
-                'precision' => 2,
-                'expected'  => '11.5 GB',
-            ],
-            [
-                'bytes'     => 123456789123456789,
-                'precision' => 2,
-                'expected'  => '112283.3 TB',
-            ],
+            [null, 2, '0.0 B'],
+            [-1, 2, '0.0 B'],
+            [0, 2, '0.0 B'],
+            [1000, 2, '1000.0 B'],
+            [1234, 2, '1.2 KB'],
+            [2345678, 2, '2.2 MB'],
+            [12345678912, 2, '11.5 GB'],
+            [123456789123456789, 2, '112283.3 TB'],
         ];
     }
 
