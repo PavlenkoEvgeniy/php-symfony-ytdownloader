e8028cacc8f3f9a221f26d8d87025e72998e3715
Pavlenko Evgeniy <pavlenkoevgeniy85@gmail.com>
2026-01-31 20:20:03 +0200

    lint

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,25 +5,24 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Entity\Log;
-use App\Entity\User;
 use App\Entity\Source;
+use App\Entity\User;
 use App\Helper\Helper;
-use Doctrine\DBAL\Exception;
 use App\Repository\LogRepository;
 use App\Repository\UserRepository;
-use App\Service\QueueCounterService;
-use App\Service\RabbitMQApiQueueService;
 use App\Service\MessengerQueueCounterService;
-use Symfony\Component\HttpFoundation\Response;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use App\Service\RabbitMQApiQueueService;
+use Doctrine\DBAL\Exception;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use Symfony\Component\Security\Core\User\UserInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 final class DashboardController extends AbstractDashboardController
