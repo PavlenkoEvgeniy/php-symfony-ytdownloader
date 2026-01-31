27aec5b6ca413fff949d212490305478b9a7c2d6
Pavlenko Evgeniy <pavlenkoevgeniy85@gmail.com>
2026-01-31 02:23:51 +0200

    add jwt passphrase generation

--- bin/generate-env.sh
+++ bin/generate-env.sh
@@ -68,6 +68,7 @@ TELEGRAM_BOT_ENABLED=false
 TELEGRAM_BOT_TOKEN=change_me_please
 TELEGRAM_HOST_URL=https://change_me_please.tld
 APP_DOWNLOADS_DIR="%kernel.project_dir%/var/downloads"
+JWT_PASSPHRASE=${JWT_PASSPHRASE_VALUE}
 EOF
 
     log "Created .env.local"
@@ -117,6 +118,7 @@ main() {
 
     APP_SECRET_VALUE="${APP_SECRET:-$(generate_secret)}"
     APP_SECRET_TEST_VALUE="${APP_SECRET_TEST:-$(generate_secret)}"
+    JWT_PASSPHRASE_VALUE="${JWT_PASSPHRASE:-$(generate_secret)}"
 
     create_env_local
     create_env_test_local
