34eb39dbc528c26f47b71d4c8ed7376664218287
Pavlenko Evgeniy <pavlenkoevgeniy85@gmail.com>
2026-07-02 06:28:46 -0400

    delete admin dashboard template

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,11 +7,6 @@ namespace App\Controller\Admin;
 use App\Entity\Log;
 use App\Entity\Source;
 use App\Entity\User;
-use App\Helper\Helper;
-use App\Repository\LogRepository;
-use App\Repository\UserRepository;
-use App\Service\MessengerQueueCounterService;
-use App\Service\RabbitMQApiQueueService;
 use Doctrine\DBAL\Exception;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
@@ -21,43 +16,19 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 final class DashboardController extends AbstractDashboardController
 {
-    public function __construct(
-        private readonly UserRepository $userRepository,
-        private readonly LogRepository $logRepository,
-        private readonly MessengerQueueCounterService $messengerQueueCounterService,
-        private readonly RabbitMQApiQueueService $rabbitMQApiQueueService,
-    ) {
-    }
-
     /**
      * @throws Exception
      */
     #[\Override]
-    public function index(): Response
+    public function index(): RedirectResponse
     {
-        $totalUsers               = $this->userRepository->getTotalCount();
-        $totalSuccessDownloads    = $this->logRepository->getTotalSuccessCount();
-        $totalPendingDownloads    = $this->messengerQueueCounterService->getQueueCount();
-        $totalInProgressDownloads = $this->rabbitMQApiQueueService->getProcessingMessagesCount();
-        $totalErrorDownloads      = $this->logRepository->getTotalErrorCount();
-        $totalSizeDownloaded      = $this->logRepository->getTotalSize();
-        $maxSizeDownloaded        = $this->logRepository->getMaxSize();
-
-        return $this->render('admin/index.html.twig', [
-            'totalUsers'      => $totalUsers,
-            'totalDownloads'  => $totalSuccessDownloads,
-            'totalPending'    => $totalPendingDownloads,
-            'totalInProgress' => $totalInProgressDownloads,
-            'totalErrors'     => $totalErrorDownloads,
-            'totalSize'       => Helper::formatBytes($totalSizeDownloaded),
-            'maxSize'         => Helper::formatBytes($maxSizeDownloaded),
-        ]);
+        return $this->redirect('/admin/user');
     }
 
     #[\Override]
@@ -86,12 +57,10 @@ final class DashboardController extends AbstractDashboardController
     #[\Override]
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToUrl('Home page', 'fa fa-home', $this->generateUrl('ui_download_index'));
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-dashboard');
         yield MenuItem::linkToCrud('Users', 'fa fa-users', User::class);
         yield MenuItem::linkToCrud('Sources', 'fa-regular fa-file-video', Source::class);
         yield MenuItem::linkToCrud('Logs', 'fa-solid fa-book', Log::class);
-        yield MenuItem::linkToLogout('Logout', 'fa fa-sign-out');
+        yield MenuItem::linkToUrl('Back to downloads', 'fa-solid fa-arrow-left', $this->generateUrl('ui_download_index'));
     }
 
     #[\Override]
