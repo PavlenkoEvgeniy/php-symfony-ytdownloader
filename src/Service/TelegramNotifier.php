e022a96e6e5dc62d7e69d5676d1fb99329a8c51d
Pavlenko Evgeniy <pavlenkoevgeniy85@gmail.com>
2026-08-14 22:41:11 +1000

    add exception annotation

--- src/Service/TelegramNotifier.php
+++ src/Service/TelegramNotifier.php
@@ -4,17 +4,25 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use BotMan\BotMan\Exceptions\Base\BotManException;
+
 final readonly class TelegramNotifier
 {
     public function __construct(private TelegramBotService $telegramBotService)
     {
     }
 
+    /**
+     * @throws BotManException
+     */
     public function notifyError(string $telegramUserId, string $message): void
     {
         $this->telegramBotService->getBot()->say($message, $telegramUserId);
     }
 
+    /**
+     * @throws BotManException
+     */
     public function notifyFinished(string $telegramUserId, string $message): void
     {
         $this->telegramBotService->getBot()->say($message, $telegramUserId);
